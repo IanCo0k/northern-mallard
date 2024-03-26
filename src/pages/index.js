315634a95c3890cee12205cc5d0dd53a67285a4a
IanCo0k <ianscook19@gmail.com>
2024-03-26 16:24:30 -0400

    please work

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ export default function App() {
   // 2. Wrap ChakraProvider at the root of your app
   return (
     <ChakraProvider className='p-4'>
+      <div></div>
       <HomePage />
     </ChakraProvider>
   )
